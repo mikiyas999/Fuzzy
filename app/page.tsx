9b98e8dd37c1e537e8dda6ea9dc5c40b024ff4eb
mikiyas <mikiyasget1234@gmail.com>
2024-03-28 18:44:04 +0000

    initial commit

--- app/page.tsx
+++ app/page.tsx
@@ -7,7 +7,6 @@ import Navbar from "@/components/global/navbar";
 import { Button } from "@/components/ui/button";
 import { clients, products } from "@/lib/constant";
 import { CheckIcon } from "lucide-react";
-import Image from "next/image";
 
 export default function Home() {
   //WIP: remove fault IMAge for home page
